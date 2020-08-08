bcd07638c1922a22f66be673e02509039c723876
lawrencedovin <lawrence@qwikwire.com>
2020-08-08 18:40:55 -0400

    Completed Day 5 Loop challenge.

---
+++ day-5-loop-challenges/lib/script.js
@@ -0,0 +1,11 @@
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    for(let i = 1; i <= 10; i++) {
+        console.log(n + " x " + i + " = " + (n*i) );
+    }
+}
