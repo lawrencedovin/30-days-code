6c2089f22e469529a4c503bfe9ec924199d2d10a
lawrencedovin <lawrence@qwikwire.com>
2020-08-22 10:46:44 -0400

    Added day 16 exception string to integer challenge.

---
+++ day-16-exceptions-string-integer/lib/script.js
@@ -0,0 +1,10 @@
+let S = "idak"
+isNaN(S) ? console.log('Bad String') : console.log(S);
+
+// try {
+//     S != Number(S) && error();
+//     console.log(Number(S));
+// } 
+// catch (error) {
+//     console.log('Bad String');
+// }
