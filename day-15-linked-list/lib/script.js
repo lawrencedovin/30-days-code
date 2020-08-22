cba2091aa40561a5eacf908a5413e48789ecb08d
lawrencedovin <lawrence@qwikwire.com>
2020-08-22 10:24:45 -0400

    Added day 15 linked list example.

---
+++ day-15-linked-list/lib/script.js
@@ -0,0 +1,37 @@
+// Input Format
+
+// The insert function has  parameters: a pointer to a Node named , and an integer value, .
+// The constructor for Node has  parameter: an integer value for the  field.
+
+// Output Format
+
+// Your insert function should return a reference to the  head node of the linked list.
+
+
+this.insert = (head,data) =>
+    {
+        //complete this method
+
+        // Create a new node with the data passed in
+        let newNode = new Node(data);
+        // Set the current node as the passed in head
+        let curNode = head;
+
+        // If the current node is not null (it has values), walk the node list
+        if (curNode != null )
+        {
+            while (curNode.next != null)
+            {
+                let curNode = curNode.next;
+            }
+            curNode.next = newNode;
+        }
+        // Otherwise, if the list is empty, return the created node
+        else
+        {
+            let head = newNode;
+        }
+    
+        return head;
+
+};
